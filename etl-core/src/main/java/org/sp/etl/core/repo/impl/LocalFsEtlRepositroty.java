0ca25c3b5b0d99fbf1d357bb4307974e2b1df303
deepak.lamba <deepak.lamba@teamcomputers.com>
2020-09-05 13:19:59 +0530

    repository updated

--- etl-core/src/main/java/org/sp/etl/core/repo/impl/LocalFsEtlRepositroty.java
+++ etl-core/src/main/java/org/sp/etl/core/repo/impl/LocalFsEtlRepositroty.java
@@ -26,24 +26,23 @@ public class LocalFsEtlRepositroty implements EtlRepositroty {
     private static final String TARGET_CONF_FILE_KEY = "targetConfigFile";
     private static final String OBJECT_CONF_FILE_KEY = "objectConfigFile";
 
-    private File jobConfFile;
-    private File dbConfFile;
-    private File sourceConfFile;
-    private File targetConfFile;
-    private File objectConfFile;
-
-    private Map<String, Job> jobs;
-    private Map<String, JsonDataObject> objectConfigs;
-    private Map<String, EtlSource> sources;
-    private Map<String, DataSource> dataSources;
-    private Map<String, EtlTarget> targets;
+    private DataStore<Job> jobDataStore;
+    private DataStore<JsonDataObject> jsonDataObjectDataStore;
+    private DataStore<EtlSource> etlSourceDataStore;
+    private DataStore<DataSource> dataSourceDataStore;
+    private DataStore<EtlTarget> etlTargetDataStore;
 
     public LocalFsEtlRepositroty(Map<String, String> parameters) {
-        this.jobConfFile = new File(parameters.get(JOB_CONF_FILE_KEY));
-        this.dbConfFile = new File(parameters.get(DB_CONF_FILE_KEY));
-        this.sourceConfFile = new File(parameters.get(SOURCE_CONF_FILE_KEY));
-        this.targetConfFile = new File(parameters.get(TARGET_CONF_FILE_KEY));
-        this.objectConfFile = new File(parameters.get(OBJECT_CONF_FILE_KEY));
+        this.jobDataStore = new DataStore<>(new File(parameters.get(JOB_CONF_FILE_KEY)), new TypeReference<List<Job>>() {
+        }, job -> job.getJobName());
+        this.jsonDataObjectDataStore = new DataStore<>(new File(parameters.get(OBJECT_CONF_FILE_KEY)), new TypeReference<List<JsonDataObject>>() {
+        }, jo -> jo.getStringValue(OBJECT_NAME_KEY));
+        this.etlSourceDataStore = new DataStore<>(new File(parameters.get(SOURCE_CONF_FILE_KEY)), new TypeReference<List<EtlSource>>() {
+        }, s -> s.sourceName());
+        this.dataSourceDataStore = new DataStore<>(new File(parameters.get(DB_CONF_FILE_KEY)), new TypeReference<List<DataSource>>() {
+        }, d -> d.dataSourceName());
+        this.etlTargetDataStore = new DataStore<>(new File(parameters.get(TARGET_CONF_FILE_KEY)), new TypeReference<List<EtlTarget>>() {
+        }, t -> t.targetName());
     }
 
     @Override
@@ -53,54 +52,56 @@ public class LocalFsEtlRepositroty implements EtlRepositroty {
 
     @Override
     public Job lookupJob(String jobName) {
-        if (this.jobs == null) {
-            this.jobs = this.readDataFile(this.jobConfFile, new TypeReference<List<Job>>() {
-            }).stream().collect(Collectors.toMap(k -> k.getJobName(), Function.identity()));
-        }
-        return this.jobs.get(jobName);
+        return this.jobDataStore.lookupElement(jobName);
     }
 
     @Override
     public EtlSource lookupEtlSource(String sourceName) {
-        if (this.sources == null) {
-            this.sources = this.readDataFile(this.sourceConfFile, new TypeReference<List<EtlSource>>() {
-            }).stream().collect(Collectors.toMap(k -> k.sourceName(), Function.identity()));
-        }
-        return this.sources.get(sourceName);
+        return this.etlSourceDataStore.lookupElement(sourceName);
     }
 
     @Override
     public EtlTarget lookupEtlTarget(String targetName) {
-        if (this.targets == null) {
-            this.targets = this.readDataFile(this.targetConfFile, new TypeReference<List<EtlTarget>>() {
-            }).stream().collect(Collectors.toMap(k -> k.targetName(), Function.identity()));
-        }
-        return this.targets.get(targetName);
+        return this.etlTargetDataStore.lookupElement(targetName);
     }
 
     @Override
     public DataSource lookupDataSource(String dataSourceName) {
-        if (this.dataSources == null) {
-            this.dataSources = this.readDataFile(this.dbConfFile, new TypeReference<List<DataSource>>() {
-            }).stream().collect(Collectors.toMap(k -> k.dataSourceName(), Function.identity()));
-        }
-        return this.dataSources.get(dataSourceName);
+        return this.dataSourceDataStore.lookupElement(dataSourceName);
     }
 
     @Override
     public JsonDataObject lookupObject(String objectName) {
-        if (this.objectConfigs == null) {
-            this.objectConfigs = this.readDataFile(this.objectConfFile, new TypeReference<List<JsonDataObject>>() {
-            }).stream().collect(Collectors.toMap(k -> k.getStringValue(OBJECT_NAME_KEY), Function.identity()));
-        }
-        return this.objectConfigs.get(objectName);
+        return this.jsonDataObjectDataStore.lookupElement(objectName);
     }
 
-    private <T> T readDataFile(File fileName, TypeReference<T> typeReference) {
-        try {
-            return JsonDataUtils.getObjectMapper().readValue(fileName, typeReference);
-        } catch (IOException e) {
-            throw new IllegalStateException("error occurred while reading data file", e);
+    private class DataStore<T> {
+        private File dataFile;
+        private Map<String, T> elements;
+
+        private Function<T, String> identifier;
+        private TypeReference<List<T>> typeReference;
+
+        public DataStore(File dataFile, TypeReference<List<T>> typeReference, Function<T, String> identifier) {
+            this.dataFile = dataFile;
+            this.typeReference = typeReference;
+            this.identifier = identifier;
+        }
+
+        public T lookupElement(String jobName) {
+            if (this.elements == null) {
+                this.elements = this.readDataFile(this.dataFile, this.typeReference)
+                        .stream().collect(Collectors.toMap(k -> this.identifier.apply(k), Function.identity()));
+            }
+            return this.elements.get(jobName);
+        }
+
+        private <T> T readDataFile(File fileName, TypeReference<T> typeReference) {
+            try {
+                return JsonDataUtils.getObjectMapper().readValue(fileName, typeReference);
+            } catch (IOException e) {
+                throw new IllegalStateException("error occurred while reading data file", e);
+            }
         }
     }
 }
